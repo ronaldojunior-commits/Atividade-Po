fd82fde068e8dacf6d54719b9f378fc3144553e6
ronaldojunior-commits <ronaldojunior311224@gmail.com>
2026-03-27 14:59:31 -0300

    Add Cachorro and Gato classes with sound methods

--- Atividade Concluida.java
+++ Atividade Concluida.java
@@ -17,3 +17,43 @@ public class Animal {
         return nome;
     }
 }
+
+public class Cachorro extends Animal{
+    String raca;
+
+    public Cachorro(String nome, String raca){
+    super(nome);
+    this.raca = raca;
+    }
+    
+    public void emitirSom(int vezes){
+        for(int i = 0; i < vezes; i++){
+            System.out.println("O cachorro late: Au Au!");
+        }
+    }
+}
+
+public class Gato extends Animal{
+    String cor;
+
+    public Gato(String nome, String cor){
+        super(nome);
+        this.cor = cor;
+    }
+    
+    public void emitirSom(){
+        System.out.println("O gato mia: Miau!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        Cachorro c1 = new Cachorro("Max", "Pastor Alemão");
+
+        Gato g1 = new Gato("Cleitinho", "Branco");
+
+        c1.emitirSom(3);
+        
+        g1.emitirSom();
+    }
+}
